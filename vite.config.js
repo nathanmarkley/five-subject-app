482d02568a8bf6219b02fd242d50d1536f0d2283
Nathan Markley <ndmarkley@gmail.com>
2026-05-14 12:40:03 -0400

    Fix Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/five-subject/',
+  base: '/five-subject-app/',
   build: {
     rollupOptions: {
       output: {
